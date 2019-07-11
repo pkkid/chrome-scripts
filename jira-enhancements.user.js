11d39ef5bac7c31259f36457d99f6c39d74eea50
Michael Shepanski <mikes@nasuni.com>
2019-07-11 14:05:06 -0400

    Update Jira View

--- jira-enhancements.user.js
+++ jira-enhancements.user.js
@@ -6,94 +6,106 @@
 // ==/UserScript==
 (function() {
     'use strict';
-  
-    // Find elements that contain text. Text is a regex /^sometext$/
-    // https://stackoverflow.com/questions/37098405
-    var contains = function(selector, text) {
-      var elements = document.querySelectorAll(selector);
-      return Array.prototype.filter.call(elements, function(element){
-        return RegExp(text).test(element.textContent);
-      });
-    };
-  
-  
-    if (document.querySelector('body').classList.contains('aui-layout')) {
-  
-      //-----------------------------------------------------------
-      // Old Bug View Changes
-      //-----------------------------------------------------------
-      console.log('--- [PK] Old Bug View Changes ---');
-      console.log('[PK] Collect important elements');
-      let agilepanel = document.querySelector('#greenhopper-agile-issue-web-panel');
-      let attachments = document.querySelector('#attachmentmodule');
-      let customfields = document.querySelector('#customfieldmodule');
-      let dates = document.querySelector('#datesmodule');
-      let description = document.querySelector('#description-val');
-      let devstatus = document.querySelector('#viewissue-devstatus-panel');
-      let people = document.querySelector('#peoplemodule');
-      let sidebar = document.querySelector('#viewissuesidebar');
-  
-      console.log('[PK] Remove stupid plugin shit!');
-      document.querySelector('div[id^="com.kanoah.test-manager"]').remove();
-      document.querySelector('div[id^="com.servicerocket.jira.salesforce"]').remove();
-      document.querySelector('#asset-fields-panel').remove();
-  
-      console.log('[PK] Remove stupid toggle titles!');
-      var titles = document.querySelectorAll('h2.toggle-title');
-      for (let i=0; i < titles.length; i++) {
-        titles[i].remove();
-      }
-  
-      console.log('[PK] Make comments not impossible to read!');
-      setInterval(function() {
-        var comments = document.querySelectorAll('.activity-comment');
-        var images = document.querySelectorAll('.activity-comment img');
-        for (let i=0; i < comments.length; i++) { comments[i].setAttribute('style', 'background-color:#F4F5F7; border:1px solid #ddd; margin-bottom:10px;'); }
-        for (let i=0; i < images.length; i++) { }
-      }, 500);
-  
-      console.log('[PK] Stop fading the description when I hover it!');
-      description.setAttribute('style', 'background-color:#F4F5F7; border:1px solid #ddd; margin-bottom:10px; padding:10px; width:100%; margin-left:0px;');
-      attachments.querySelector('#attachmentmodule_heading').remove();
-      attachments.setAttribute('style', 'padding:0px; margin:0px;');
-  
-      console.log('[PK] Move custom fields to the sidebar!');
-      dates.parentNode.insertBefore(customfields, devstatus);
-      var items = customfields.querySelectorAll('li.item');
-      var values = customfields.querySelectorAll('div.value');
-      var labels = sidebar.querySelectorAll('.item-details dl>dt');
-      for (let i=0; i < items.length; i++) { items[i].setAttribute('style', 'margin:10px 0px;'); }
-      for (let i=0; i < values.length; i++) { values[i].setAttribute('style', 'min-height:20px; min-width:60px;'); }
-      for (let i=0; i < labels.length; i++) { labels[i].setAttribute('style', 'width:150px;'); }
-      agilepanel.setAttribute('style', 'padding:0px; margin:0px;');
-  
-      console.log('[PK] Disable click to edit description bullshit!');
-      setTimeout(function() {
-        var descriptionClone = description.cloneNode(true);
-        description.parentNode.replaceChild(descriptionClone, description);
-        descriptionClone.addEventListener("dblclick", function() {
-          descriptionClone.parentNode.replaceChild(description, descriptionClone);
-          setTimeout(function() { description.click(); }, 200);
+
+    setTimeout(function() {
+      
+      // Find elements that contain text. Text is a regex /^sometext$/
+      // https://stackoverflow.com/questions/37098405
+      var contains = function(selector, text) {
+        var elements = document.querySelectorAll(selector);
+        return Array.prototype.filter.call(elements, function(element){
+          return RegExp(text).test(element.textContent);
         });
-      }, 1000);
-    } else {
-  
-      //-----------------------------------------------------------
-      // New Bug View Changes
-      //-----------------------------------------------------------
+      };
+
+      // Find the parent with the specified cls
+      var findParent = function(elem, cls) {
+        elem = elem.parentElement;
+        while ((elem.className != cls) && (elem.nodeName != 'BODY')) {
+          elem = elem.parentElement;
+        }
+        return elem;
+      };
+
+      // Find the top item still within parent
+      var gotoParent = function(elem, parent) {
+        while ((elem.parentElement != parent) && (elem.nodeName != 'BODY')) {
+          elem = elem.parentElement;
+        }
+        return elem;
+      };
+
+      // Extract a single className from the element
+      var className = function(elem, maxLength) {
+        maxLength = maxLength || 6;
+        var classNames = elem.className.split(' ');
+        for (var i=0; i<classNames.length; i++) {
+          if (classNames[i].length <= maxLength) {
+            return classNames[i];
+          }
+        }
+        return null;
+      };
+
+      // Add Custom Styles
       console.log('--- [PK] New Bug View Changes ---');
-  
-      console.log('[PK] Adding Custom Stylesheet');
+      console.log('[PK] Finding classnames..');
+      var status = findParent(contains('h2', /^Status$/)[0], '');
+      var reporter = findParent(contains('h2', /^Reporter$/)[0], '');
+      var components = findParent(contains('h2', /^Components$/)[0], '');
+      var sideBar = status.parentElement;
+      var sideBarCls = className(sideBar);
+      var labelCls = className(status.childNodes[0]);
+      var statusCls = className(status.childNodes[1]);
+      var peopleCls = className(reporter.childNodes[1]);
+      var valueCls = className(components.childNodes[1]);
+      var sideBarValues = components.parentElement;
+      var sideBarValuesCls = className(sideBarValues);
+      console.log('  sideBarCls: '+ sideBarCls);
+      console.log('  sideBarValuesCls: '+ sideBarValuesCls);
+      console.log('  labelCls: '+ labelCls);
+      console.log('  statusCls: '+ statusCls);
+      console.log('  peopleCls: '+ peopleCls);
+      console.log('  valueCls: '+ valueCls);
+      console.log('[PK] Adding Custom Stylesheet..');
       var sheet = document.createElement('style');
       sheet.innerHTML = `
-        .iGyYHq .dAtTjS { float:left; width:150px; padding-top:12px; }  /* Sidebar headers */
-        .iGyYHq .igPjYz { padding-top: 12px; padding-bottom: 12px; }    /* Status chooser */
-        .iGyYHq .ehGzOz { margin-left:150px; margin-bottom:5px; }       /* People pickers */
-        .iGyYHq .gKBQVf { margin-left:150px; margin-bottom:5px; }       /* Regular fields */
+        .${sideBarCls} .${labelCls} { float:left; width:150px; padding-top:12px; }
+        .${sideBarCls} .${statusCls} { padding-top:12px; padding-bottom:12px; }
+        .${sideBarCls} .${peopleCls} { margin-left:150px; margin-bottom:5px; }
+        .${sideBarCls} .${valueCls} { margin-left:150px; margin-bottom:5px; }
+        .${sideBarValuesCls} > div { margin-bottom:0px !important; }
       `;
       document.body.appendChild(sheet);
-    }
-  
+
+      // Hide None Values
+      console.log('[PK] Hide none values..');
+      var noneValues = contains(`.${sideBarValuesCls} span`, /^None$/);
+      for (let i=0; i<noneValues.length; i++) {
+        noneValues[i] = gotoParent(noneValues[i], sideBarValues);
+        noneValues[i].style.display = 'none';
+      }
+      // Add Menu Item to Show Hidden Values
+      console.log('[PK] Show hidden values when clicking Show more');
+      var showMore = contains('span', /^Show more$/)[0];
+      console.log(showMore);
+      showMore.addEventListener('click', function() {
+        console.log('CLICK!!!');
+        for (let i=0; i<noneValues.length; i++) {
+          noneValues[i].style.display = 'block';
+        }
+      });
+
+      // Require Doubleclick to Edit Description..
+      console.log('[PK] Require doubleclick to edit description..');
+      var description = findParent(contains('h2', /^Description$/)[0], '');
+      var descriptionClone = description.cloneNode(true);
+      description.parentNode.replaceChild(descriptionClone, description);
+      descriptionClone.addEventListener("dblclick", function() {
+        descriptionClone.parentNode.replaceChild(description, descriptionClone);
+        setTimeout(function() { description.click(); }, 400);
+      });
+
+    }, 1000);
   })();
-  
   
