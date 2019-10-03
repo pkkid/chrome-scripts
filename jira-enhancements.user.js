1ecad0ee4688ec519563eb84f46c7636ddfc7275
Michael Shepanski <mikes@nasuni.com>
2019-10-03 10:18:18 -0400

    More enhancements

--- jira-enhancements.user.js
+++ jira-enhancements.user.js
@@ -7,7 +7,8 @@
 (function() {
   'use strict';
 
-  setTimeout(function() {
+  var fixEverything = function(event) {
+    event.preventDefault();
     
     // Find elements that contain text. Text is a regex /^sometext$/
     // https://stackoverflow.com/questions/37098405
@@ -36,7 +37,7 @@
     };
 
     // Extract a single className from the element
-    var className = function(elem, maxLength) {
+    var cls = function(elem, maxLength) {
       maxLength = maxLength || 6;
       var classNames = elem.className.split(' ');
       for (var i=0; i<classNames.length; i++) {
@@ -47,89 +48,109 @@
       return null;
     };
 
-    // Spelunk through the DOM to find a few key class names
-    console.log('--- [PK] New Bug View Changes ---');
-    console.log('[PK] Finding classnames..');
-    var status = contains('h2', /^Status$/)[0].parentElement.parentElement;  // Status wrapper (inside sidebar)
-    var statusCls = className(status.childNodes[1]);
-      console.log('  statusCls: '+ statusCls);
-    var labelCls = className(status.childNodes[0]);
-      console.log('  labelCls: '+ labelCls);
-    var sideBar = status.parentElement;
-    var sideBarCls = className(sideBar);
-      console.log('  sideBarCls: '+ sideBarCls);
-    var components = findParent(contains('h2', /^Team Assigned$/)[0], '');
-    var sideBarValues = components.parentElement;
-    var sideBarValuesCls = className(sideBarValues);
-      console.log('  sideBarValuesCls: '+ sideBarValuesCls);
-    var valueCls = className(components.childNodes[1].childNodes[0]);
-      console.log('  valueCls: '+ valueCls);
-    var reporter = findParent(contains('h2', /^Reporter$/)[0], '');
-    var peopleCls = className(reporter.childNodes[1]);
-      console.log('  peopleCls: '+ peopleCls);
-    var description = contains('h2', /^Description$/)[0].parentElement.parentElement;
-    var descriptionCls = className(description);
-      console.log('  descriptionCls: '+ descriptionCls);
-    var content = description.parentElement;
-    var contentValuesCls = className(description.nextSibling.childNodes[0]);
-      console.log('  contentValuesCls: '+ contentValuesCls);
-    
-    // Create a custom stylesheet
-    console.log('[PK] Adding Custom Stylesheet..');
-    var sheet = document.createElement('style');
-    sheet.innerHTML = `
-      .${sideBarCls} .${labelCls} { float:left; width:150px; padding-top:12px; }
-      .${sideBarCls} .${statusCls} { padding-top:12px; padding-bottom:12px; }
-      .${sideBarCls} .${peopleCls} { margin-left:150px; margin-bottom:5px; }
-      .${sideBarCls} .${valueCls} { margin-left:150px; margin-bottom:5px; }
-      .${sideBarValuesCls} > div { margin-bottom:0px !important; }
-      div[class^="Content-"]:hover { background-color: transparent !important; }
-    `;
-    document.body.appendChild(sheet);
+    var
+    _components,
+    _reporter,
+    description,
+    mainPanel,
+    status,
+    sidePanel,
+    sidePanelValues,
+    labelCls,
+    peopleCls,
+    valueCls,
+    developmentCls;
 
-    // Hide None Values in Sidebar
-    console.log('[PK] Hide none values..');
-    var hidden = [];
-    var noneValues = contains(`.${sideBarValuesCls} span`, /^None$/);
-    for (let i=0; i<noneValues.length; i++) {
-      let elem = gotoParent(noneValues[i], sideBarValues);
-      if (elem.nodeName != 'BODY') {
-        elem.style.display = 'none';
-        hidden.push(elem);
-      }
+    // SidePanel - Spelunk through the DOM to find a few key class names
+    try {
+      console.log('[PK] --> Finding classnames..');
+      _components = findParent(contains('h2', /^Team Assigned$/)[0], ''); console.log(_components);
+      _reporter = findParent(contains('h2', /^Reporter$/)[0], ''); console.log(_reporter);
+      description = contains('h2', /^Description$/)[0].parentElement.parentElement; console.log('[PK] description: '+ cls(description));
+      mainPanel = description.parentElement; console.log('[PK] mainPanel: '+ cls(mainPanel));
+      status = contains('h2', /^Status$/)[0].parentElement.parentElement; console.log('[PK] status: '+ cls(status));
+      sidePanel = status.parentElement.parentElement; console.log('[PK] sidePanel: '+ cls(sidePanel));
+      sidePanelValues = _components.parentElement; console.log('[PK] sidePanelValues: '+ cls(sidePanelValues));
+      labelCls = cls(status.childNodes[0]); console.log('[PK] labelCls: '+ labelCls);
+      peopleCls = cls(_reporter.childNodes[1]); console.log('[PK] peopleCls: '+ peopleCls);
+      valueCls = cls(_components.childNodes[1].childNodes[0]); console.log('[PK] valueCls: '+ valueCls);
+      developmentCls = cls(contains('h2', /^Development$/)[0]); console.log('[PK] developmentCls: '+ developmentCls);
+    } catch(err) {
+      console.log('[PK] ERROR: '+ err.message);
     }
-    // Hide None Value in Content
-    noneValues = contains(`.${contentValuesCls} span`, /^None$/);
-    for (let i=0; i<noneValues.length; i++) {
-      let elem = gotoParent(noneValues[i], content);
-      if (elem.nodeName != 'BODY') {
-        elem.style.display = 'none';
-        hidden.push(elem);
-      }
+
+    // Create a custom stylesheet
+    try {
+      console.log('[PK] --> Adding Custom Stylesheet..');
+      var sheet = document.createElement('style');
+      sheet.innerHTML = `
+        .${cls(sidePanel)} .${labelCls} { float:left; width:150px; padding-top:12px; }
+        .${cls(sidePanel)} .${cls(status)} { padding-top:45px; margin-bottom:-16px; }
+        .${cls(sidePanel)} .${peopleCls} { margin-left:150px; margin-bottom:5px; }
+        .${cls(sidePanel)} .${valueCls} { margin-left:150px; margin-bottom:5px; }
+        .${cls(sidePanel)} .${developmentCls} { padding-top: 12px; }
+        .${cls(sidePanelValues)} > div { margin-bottom:0px !important; }
+        /* div[class^="LayoutStyles__Container-"] { background-color:#f8f8f8; padding:5px; border:1px solid #eee; border-radius:4px; margin-bottom:5px; } */
+        div[class^="Content-"]:hover { background-color: transparent !important; }
+      `;
+      document.body.appendChild(sheet);
+    } catch(err) {
+      console.log('[PK] ERROR: '+ err.message);
+    }
+
+    // Append title to fixed header
+    try {
+      console.log('[PK] --> Appending issue title to fixed header..');
+      var title = document.title.replace(' - JIRA', '').split('] ')[1]; console.log('[PK] title: '+ title);
+      var issue = document.title.split(']')[0].replace('[', ''); console.log('[PK] issue: '+ issue);
+      var headers = contains('span', /^UNTY/);
+      var header = headers[headers.length - 1].parentElement.parentElement;  console.log('[PK] header: '+ cls(header));
+      header.text = issue +' - '+ title;
+    } catch(err) {
+      console.log('[PK] ERROR: '+ err.message);
     }
     
-    // Add Menu Item to Show Hidden Values
-    console.log('[PK] Show hidden values when clicking Show more');
-    var showMore = contains('span', /^Show more$/)[0];
-    showMore.addEventListener('click', function() {
-      for (let i=0; i<hidden.length; i++) {
-        hidden[i].style.display = 'block';
+    // Hide None Values in Sidebar
+    try {
+      console.log('[PK] --> Hide None values..');
+      var hidden = [];
+      var panels = [sidePanelValues, mainPanel];
+      for (let p=0; p<panels.length; p++) {
+        let nones = contains(`.${cls(panels[p])} span`, /^None$/);
+        for (let i=0; i<nones.length; i++) {
+          let elem = gotoParent(nones[i], panels[p]);
+          if (elem.nodeName != 'BODY') {
+            elem.style.display = 'none';
+            hidden.push(elem);
+          }
+        }
       }
-    });
+    } catch(err) {
+      console.log('[PK] ERROR: '+ err.message);
+    }
 
     // Require Doubleclick to Edit Description..
-    console.log('[PK] Require doubleclick to edit description..');
-    var description = findParent(contains('h2', /^Description$/)[0], '');
-    var descriptionClone = description.cloneNode(true);
-    description.parentNode.replaceChild(descriptionClone, description);
-    descriptionClone.addEventListener("dblclick", function() {
-      descriptionClone.parentNode.replaceChild(description, descriptionClone);
-      setTimeout(function() { description.click(); }, 400);
-    });
+    // console.log('[PK] --> Require doubleclick to edit description..');
+    // var descriptionBox = findParent(contains('h2', /^Description$/)[0], '');
+    // var descriptionClone = descriptionBox.cloneNode(true);
+    // descriptionBox.parentNode.replaceChild(descriptionClone, descriptionBox);
 
-    // Hide BETA Warning
-    //console.log('[PK] Hide beta warning..');
-    //document.querySelector('#jira-issue-header').previousSibling.style.display = 'none';
+    // Doubleclick to Undo everything!
+    // document.addEventListener('dblclick', function() {
+    //   console.log('[PK] --> Undo description..');
+    //   descriptionClone.parentNode.replaceChild(descriptionBox, descriptionClone);
+    //   console.log('[PK] --> Show Hidden feilds..');
+    //   for (let i=0; i<hidden.length; i++) { hidden[i].style.display = 'block'; }
+    // });
 
-  }, 1000);
+  };
+
+  var fixedit = false;
+  document.addEventListener('dblclick', function(event) {
+    if (!fixedit) {
+      fixEverything(event);
+      fixedit = true;
+    }
+  });
+
 })();
